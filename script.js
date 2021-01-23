ae3efcefb85b46d44dc37d710db3392aafdf68d2
Stefanie Nader <stefanie.nader1992@web.de>
2021-01-23 18:10:03 +0100

    first steps

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Start the Game
+ */
+
+function startQuiz() {
+    document.getElementById('welcome').classList.add('d-none');
+    document.getElementById('quiz-container').classList.remove('d-none');
+    document.getElementById('quiz-container').classList.add('quiz-container');
+}
